325e548edae905b47a3a0e1c5d6c794e1867fc60
leonel-carvajal <liosoft777@gmail.com>
2020-12-01 15:36:38 -0600

    haciendo diseño mobile

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const menu = document.getElementById('menu')
+const ham = document.querySelector('.navbar__ham')
+ham.addEventListener('click', () => {
+  if (menu.classList.contains('menu') && !menu.classList.contains('show')) {
+    menu.classList.add('show')
+    ham.src = '/images/icon-close.svg'
+  } else {
+    menu.classList.remove('show')
+    ham.src = '/images/icon-hamburger.svg'
+  }
+})
+
+
 new Splide('.splide', {
   type: 'loop',
   focus: 'center',
