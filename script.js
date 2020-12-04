7825cec64bb884d540bf6be5338f789a40ebda12
leonel-carvajal <liosoft777@gmail.com>
2020-12-03 19:52:28 -0600

    version desktop

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const menu = document.getElementById('menu')
 const ham = document.querySelector('.navbar__ham')
-const send = document.getElementById('send')
-const email = document.getElementById('email')
-const msj = document.getElementById('mensaje')
 ham.addEventListener('click', () => {
   if (menu.classList.contains('menu') && !menu.classList.contains('show')) {
     menu.classList.add('show')
@@ -13,29 +10,24 @@ ham.addEventListener('click', () => {
   }
 })
 
-send.addEventListener('click', (e) => {
-  const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+let number = 0
+console.log(number)
+if (window.matchMedia("(max-width:360px)").matches) {
+  number = 1
+}
+if (!window.matchMedia("(max-width:360px)").matches) {
+  number = 3
+}
 
-  if (emailRegex.test(email.value)) {
-    console.log('email válido')
-    msj.textContent = 'Email send correctly'
-    msj.classList.toggle('error')
-  }
-  else {
-    console.log('email incorrecto')
-    msj.textContent = 'Email not valid'
-    msj.classList.toggle('error')
-    email.value = ''
-  }
-})
-
-new Splide('.splide', {
+new Splide('.splide',
+  {
   type: 'loop',
   focus: 'center',
-  gap: '20px',
-  perPage: 1,
+  gap: '50px',
+  perPage: number,
   perMove: 1,
   keyboard: true,
   speed:1000
-}).mount(
+  }
+).mount(
 );
