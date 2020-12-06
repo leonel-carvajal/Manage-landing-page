d22e802c9155fd05c5c4ad0ecfca3d191b068d58
leonel-carvajal <liosoft777@gmail.com>
2020-12-05 18:01:25 -0600

    validando email

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menu = document.getElementById('menu')
 const ham = document.querySelector('.navbar__ham')
+const email = document.getElementById('email')
+
 ham.addEventListener('click', () => {
   if (menu.classList.contains('menu') && !menu.classList.contains('show')) {
     menu.classList.add('show')
@@ -9,16 +11,22 @@ ham.addEventListener('click', () => {
     ham.src = '/images/icon-hamburger.svg'
   }
 })
-
 let number = 0
-console.log(number)
 if (window.matchMedia("(max-width:360px)").matches) {
   number = 1
 }
 if (!window.matchMedia("(max-width:360px)").matches) {
   number = 3
 }
-
+const validateEmail = (email) => {
+  const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (emailRegex.test(email)) alert('Email Valid')
+  else alert('Not Valid Email')
+}
+const comenzar = () => {
+  const send = document.getElementById('send')
+  send.addEventListener('click',validateEmail)
+}
 new Splide('.splide',
   {
   type: 'loop',
@@ -31,3 +39,4 @@ new Splide('.splide',
   }
 ).mount(
 );
+window.addEventListener('DOMContentLoaded',comenzar,false)
